5f45a3ea1b8de3fea4e07ec9b8c10838a6fc72c3
OfirGilad <ofirgila@post.bgu.ac.il>
2020-12-23 11:15:12 +0200

    client Cmake create

---
+++ Client/src/cmake-build-debug/CMakeFiles/spl-03.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/spl-03.dir/CRSClient.cpp.o"
+  "spl-03"
+  "spl-03.pdb"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/spl-03.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
