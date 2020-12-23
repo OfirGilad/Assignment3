924870204fdac4b77302754c4616c2c4910998d8
OfirGilad <63189277+OfirGilad@users.noreply.github.com>
2020-12-23 17:25:46 +0200

    Delete CRSMessagingProtocolInterface.java

--- Server/src/main/java/bgu/spl/net/api/CRSMessagingProtocolInterface.java
+++
@@ -1,8 +0,0 @@
-package bgu.spl.net.api;
-
-public interface CRSMessagingProtocolInterface<T> extends MessagingProtocol<T>{
-
-    void start(String userName);
-    T process(T msg);
-    boolean shouldTerminate();
-}
