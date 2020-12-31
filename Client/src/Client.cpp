53dd94236853d5c5eedb3328e03a9cfa9957c90a
ohad <sharet@post.bgu.ac.il>
2020-12-31 13:47:44 +0200

    tester added

--- Client/src/Client.cpp
+++ Client/src/Client.cpp
@@ -10,15 +10,13 @@
 using namespace std;
 
 int main (int argc, char *argv[]) {
-    //if (argc < 3) {
-    //    std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
-    //    return -1;
-    //}
-    //std::string host = argv[1];
-    //short port = atoi(argv[2]);
+    if (argc < 3) {
+        std::cerr << "Usage: " << argv[0] << " host port" << std::endl << std::endl;
+        return -1;
+    }
+    std::string host = argv[1];
+    short port = atoi(argv[2]);
 
-    string host = "132.72.229.220";
-    short port = 7777;
     ConnectionHandler connectionHandler(host, port);
     if (!connectionHandler.connect()) {
         std::cerr << "Cannot connect to " << host << ":" << port << std::endl;
